afd88476972e5877c802a12f583ea2095cafe8d7
SKORNEV74 <yasher7495@yandex.ru>
2023-03-15 02:07:34 +0300

    improvements

--- task7.js
+++ task7.js
@@ -3,8 +3,7 @@ let arr7 = [1, 2, 4, 0, null, "5", {}, undefined, NaN, true, "", []];
 let odd = 0, even = 0, zero = 0, other = 0;
 
 arr7.forEach(function (item, index, array){
-    if (isNaN(item) || item === undefined || item === null ||
-        typeof item === "string" || typeof item === "object" || typeof item === "boolean") {
+    if (isNaN(item) || typeof item !== "number") {
         other++;
     } else if (item === 0) {
         zero++;
